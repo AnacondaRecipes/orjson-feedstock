0157e460948dc522a4ed5007a9309442c750de77
Jean-Christophe Morin <jcmorin@anaconda.com>
2023-12-18 13:23:24 -0500

    Update to 3.9.10

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,6 +7,6 @@ maturin build --release --strip --manylinux off --interpreter=%PYTHON%
 
 FOR /F "delims=" %%i IN ('dir /s /b target\wheels\*.whl') DO set orjson_wheel=%%i
 
-%PYTHON% -m pip install --no-deps %orjson_wheel% -vv
+%PYTHON% -m pip install --no-deps %orjson_wheel% -vv --no-build-isolation
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
